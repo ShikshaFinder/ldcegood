6a02e110d3c24ea84bf0a247452af6cccacc655b
ShikshaFinder <janiharsh794@gmail.com>
2024-04-03 18:38:27 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "aceternity.com",
+      "unsplash.com",
+      "images.unsplash.com",
+      "plus.unsplash.com",
+    ],
+  },
+};
 
 export default nextConfig;
