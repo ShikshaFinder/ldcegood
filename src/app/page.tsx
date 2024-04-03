822be253c044791e42fc39aeba5bec461d5295a5
ShikshaFinder <janiharsh794@gmail.com>
2024-04-03 21:15:27 +0530

    error correcrted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { HeroParallax } from "../components/ui/hero-parallax";
 import { Button } from "../components/ui/moving-border";
+import Link from "next/link";
 
 export default function HeroParallaxDemo() {
   return (
     <>
-      <Button
+      <Link href="/page"> <Button
         borderRadius="1.75 rem"
         className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800 "
       >
         Shiksha Finder
-      </Button>
+      </Button></Link>
       <HeroParallax products={products} />
     </>
   );
