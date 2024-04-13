4682a9b15095fe65a0eec63a98119b672aa09306
ShikshaFinder <janiharsh794@gmail.com>
2024-04-13 14:04:48 +0530

    reversed clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 
 
 const inter = Inter({ subsets: ["latin"] });
